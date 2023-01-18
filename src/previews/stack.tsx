bbaf9b0a50ab8779ed029c907f5db9a77afcb01a
hirano <null>
2023-01-18 14:08:21 +0900

    refactoring: removed unused previews

--- src/previews/stack.tsx
+++ src/previews/stack.tsx
@@ -1,4 +1,4 @@
-import {createSignal, createEffect, on, Show} from 'solid-js';
+import {createEffect, on} from 'solid-js';
 import type { Component } from 'solid-js';
 import * as glutil from 'glutil';
 
@@ -15,28 +15,17 @@ const Stack: Component<{
   let resolution: [number, number] = [props.src.width, props.src.height];
 
   let context = canvas.getContext("webgl2")!;
-
   const srcTexture = context.createTexture()!;
-
   let effect = new EffectStack(context, srcTexture, null, resolution);
-
-  const title = "EffectStack";
 
   createEffect(on(props.update, () => {
     glutil.loadTexture(props.src, srcTexture, context);
     effect.update();
   }));
-  const [visible, setVisibile] = createSignal(false);
-  const toggleVisible = () => {setVisibile(!visible())};
   return (
     <div>
-      <div>
-        <a onClick={toggleVisible} href="#">{title}</a>
-      </div>
-      <Show when={visible()}>
-        {effect.ui({})}
-        {signalingCanvas(undefined)}
-      </Show>
+      {effect.ui({})}
+      {signalingCanvas(undefined)}
     </div>
   );
 
