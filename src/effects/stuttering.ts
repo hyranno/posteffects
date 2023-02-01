470957b32b43a45f978b6f4418e11d810e8b1ee0
hirano <null>
2023-02-01 11:36:35 +0900

    refactoring: efficient Stuttering

--- src/effects/stuttering.ts
+++ src/effects/stuttering.ts
@@ -6,8 +6,7 @@ import {NopShader} from 'effects/nop';
 export class StutteringEffect extends glutil.GlEffect {
   rand: Random;
   ratio: number;
-  nopToState: NopShader;
-  nopToDest: NopShader;
+  nop: NopShader;
   constructor(
     context: WebGL2RenderingContext,
     src: WebGLTexture,
@@ -17,30 +16,22 @@ export class StutteringEffect extends glutil.GlEffect {
     ratio: number,
   ) {
     super(context);
-    let stateTexture = glutil.createBufferTexture(context, resolution);
-    let stateBuffer = glutil.bindNewFramebuffer(context, stateTexture);
-
-    let nop_to_state = new NopShader(context, src, stateBuffer, resolution);
-    let nop_to_dest = new NopShader(context, stateTexture, dest, resolution);
-    nop_to_state.update();
 
     this.rand = new Random(seed, 0);
     this.ratio = ratio;
-    this.nopToState = nop_to_state;
-    this.nopToDest = nop_to_dest;
+    this.nop = new NopShader(context, src, dest, resolution);
   }
 
   override update(){
     if (this.ratio < this.rand.random()) {
-      this.nopToState.update();
+      this.nop.update();
     }
-    this.nopToDest.update();
   }
   setSrc(src: WebGLTexture): void {
-    this.nopToState.src = src;
+    this.nop.src = src;
   }
   setDest(dest: WebGLFramebuffer | null): void {
-    this.nopToDest.dest = dest;
+    this.nop.dest = dest;
   }
   setSeed(seed: number) {
     this.rand = new Random(seed, 0);
